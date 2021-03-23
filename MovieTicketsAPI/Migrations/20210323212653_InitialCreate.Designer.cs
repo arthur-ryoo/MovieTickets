a8bbeeb6c12c50420c66ef080689d2ebfd65ff04
Arthur Ryoo <jene0523@gmail.com>
2021-03-23 14:30:34 -0700

    Migrate database

--- MovieTicketsAPI/Migrations/20210323212653_InitialCreate.Designer.cs
+++ MovieTicketsAPI/Migrations/20210323212653_InitialCreate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieTicketsAPI.Data;
 
 namespace MovieTicketsAPI.Migrations
 {
     [DbContext(typeof(MovieDbContext))]
-    partial class MovieDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210323212653_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
