7fb2373131c4498536ab5a2c41786156471de08e
Arthur Ryoo <jene0523@gmail.com>
2021-03-23 13:58:27 -0700

    Add project files.

---
+++ MovieTicketsAPI/Data/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieTicketsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTicketsAPI.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ MovieTicketsAPI/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTicketsAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name cannot be null or empty")]
+        public string Name { get; set; }
+        [Required]
+        public string Language { get; set; }
+        [Required]
+        public double Rating { get; set; }
+        [NotMapped]
+        public IFormFile Image { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
