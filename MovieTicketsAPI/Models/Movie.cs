05f125aa08e8c491204275f523296d6be7be5aaa
Arthur Ryoo <jene0523@gmail.com>
2021-03-23 14:30:53 -0700

    Edit Movie model

--- MovieTicketsAPI/Models/Movie.cs
+++ MovieTicketsAPI/Models/Movie.cs
@@ -11,14 +11,19 @@ namespace MovieTicketsAPI.Models
     public class Movie
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "Name cannot be null or empty")]
         public string Name { get; set; }
-        [Required]
+        public string Description { get; set; }
         public string Language { get; set; }
-        [Required]
+        public string Duration { get; set; }
+        public DateTime PlayingDate { get; set; }
+        public DateTime PlayingTime { get; set; }
+        public double TicketPrice { get; set; }
         public double Rating { get; set; }
+        public string Genre { get; set; }
+        public string TrailorUrl { get; set; }
+        public string ImageUrl { get; set; }
         [NotMapped]
         public IFormFile Image { get; set; }
-        public string ImageUrl { get; set; }
+        public ICollection<Reservation> Reservations { get; set; }
     }
 }
