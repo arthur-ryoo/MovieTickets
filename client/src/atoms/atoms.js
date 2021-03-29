2aad33cd4139cf5c7d2c3a023fcb06b22f133b23
ArthurRyoo <jene0523@gmail.com>
2021-03-28 23:20:13 -0700

    Add recoil-persist

--- client/src/atoms/atoms.js
+++ client/src/atoms/atoms.js
@@ -1,9 +1,13 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+const { persistAtom } = recoilPersist();
 
 export const userAtom = atom({
   key: 'user',
   default: {
     id: 0,
     role: '',
+    isLoggined: false,
   },
+  effects_UNSTABLE: [persistAtom],
 });
