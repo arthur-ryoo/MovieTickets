58d8d1bb378f5c918e9a4673fc556ad998b47846
ArthurRyoo <jene0523@gmail.com>
2021-03-31 00:11:02 -0700

    Add admin path

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,26 @@ import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
 import Home from './pages/Home/Home';
 import MovieDetails from './components/MovieDetails/MovieDetails';
+import ReservationList from './components/Admin/ReservationList/ReservationList';
+import MovieList from './components/Admin/MovieList/MovieList';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/movies/:id" component={MovieDetails} />
-      </Switch>
-      <Footer />
+      <div className="container">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/movies/:id" component={MovieDetails} />
+          <Route exact path="/admin/reservations" component={ReservationList} />
+          <Route exact path="/admin/movies" component={MovieList} />
+        </Switch>
+      </div>
+      <div className="subContainer">
+        <Footer />
+      </div>
     </div>
   );
 }
