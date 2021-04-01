dfbab72605bc2565b76eb699047e687e77bbcd19
ArthurRyoo <jene0523@gmail.com>
2021-04-01 00:31:29 -0700

    Add display logic

--- client/src/components/MovieDetails/Sections/MovieBookingForm.js
+++ client/src/components/MovieDetails/Sections/MovieBookingForm.js
@@ -1,5 +1,12 @@
 import React, { useReducer } from 'react';
-import { Grid, Typography, TextField, Button, Box } from '@material-ui/core';
+import {
+  Grid,
+  Typography,
+  TextField,
+  Button,
+  Box,
+  Link,
+} from '@material-ui/core';
 import { axiosApiInstance as API } from '../../../utils/axiosConfig';
 import { useRecoilValue } from 'recoil';
 import { userAtom } from '../../../atoms/atoms';
@@ -50,46 +57,67 @@ export default function MovieBookingForm(props) {
     props.handleClose();
   };
   return (
-    <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Booking Info
-      </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="phone"
-            name="phone"
-            label="Phone Number"
-            type="text"
-            fullWidth
-            value={state.phone}
-            onChange={handleChange}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="qty"
-            name="qty"
-            label="Number of Tickets"
-            type="number"
-            fullWidth
-            value={state.qty}
-            onChange={handleChange}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="h6" color="primary" onChange={handleChange}>
-            Total: ${props.movieDetail.ticketPrice * state.qty}
+    <div>
+      {user.isLoggedIn ? (
+        <React.Fragment>
+          <Typography variant="h6" gutterBottom>
+            Booking Info
           </Typography>
-        </Grid>
-        <Box marginLeft="10px" marginBottom="30px">
-          <Button variant="contained" color="primary" onClick={handleSubmit}>
-            Book
-          </Button>
-        </Box>
-      </Grid>
-    </React.Fragment>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="phone"
+                name="phone"
+                label="Phone Number"
+                type="text"
+                fullWidth
+                value={state.phone}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="qty"
+                name="qty"
+                label="Number of Tickets"
+                type="number"
+                fullWidth
+                value={state.qty}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h6" color="primary" onChange={handleChange}>
+                Total: ${props.movieDetail.ticketPrice * state.qty}
+              </Typography>
+            </Grid>
+            <Box marginLeft="10px" marginBottom="30px">
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleSubmit}
+              >
+                Book
+              </Button>
+            </Box>
+          </Grid>
+        </React.Fragment>
+      ) : (
+        <div>
+          <Typography variant="h6" color="primary">
+            Login to book your ticket
+          </Typography>
+          <Box textAlign="center" marginY="30px">
+            <Link href="/login" underline="none">
+              <Button variant="contained" color="primary">
+                Login
+              </Button>
+            </Link>
+          </Box>
+        </div>
+      )}
+    </div>
   );
 }
